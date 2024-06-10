86c8aede8eedabb149a2e5de49dfaf635a6f8271
Daniel M <85372728+Overclocker2077@users.noreply.github.com>
2024-06-09 20:13:32 -0400

    Add files via upload

---
+++ Game_Art/Maps/Map1/Crater_88x88.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Crater_88x88" tilewidth="88" tileheight="88" tilecount="1" columns="1">
+ <image source="Crater_88x88.jpg" width="88" height="88"/>
+</tileset>
---
+++ Game_Art/Maps/Map1/Stone_Crater_87x87.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Stone_Crater_87x87" tilewidth="87" tileheight="87" tilecount="1" columns="1">
+ <image source="Stone_Crater_87x87.jpg" width="87" height="87"/>
+</tileset>
